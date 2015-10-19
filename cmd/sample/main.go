fea7922616485dd66f8328df09c0511e7ffeff32
Steve Yen <steve.yen@gmail.com>
2015-10-19 08:15:37 -0700

    rename test to sample

---
+++ cmd/sample/main.go
@@ -0,0 +1,44 @@
+//  Copyright (c) 2015 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the
+//  License. You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+//  applicable law or agreed to in writing, software distributed under
+//  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+//  OR CONDITIONS OF ANY KIND, either express or implied. See the
+//  License for the specific language governing permissions and
+//  limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	bleveMappingUI "github.com/blevesearch/bleve-mapping-ui"
+
+	// import general purpose configuration
+	_ "github.com/blevesearch/bleve/config"
+)
+
+var bindAddr = flag.String("bindAddr", ":9090", "http listen address")
+
+func main() {
+	flag.Parse()
+
+	router := mux.NewRouter()
+
+	router.StrictSlash(true)
+
+	bleveMappingUI.RegisterHandlers(router, "/api")
+
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir(".")))
+
+	// start the HTTP server
+	http.Handle("/", router)
+	log.Printf("Listening on %v", *bindAddr)
+	log.Fatal(http.ListenAndServe(*bindAddr, nil))
+}
---
+++ sample.js
@@ -0,0 +1,78 @@
+angular.module('sampleApp', ['ui.tree', 'ui.bootstrap']);
+
+var SAMPLE_TYPE_MAPPING = {
+    "brewery": {
+        "enabled": true,
+        "properties": {
+            "name": {
+                "fields":[{"name":"name","type":"text","analyzer":"en"}],
+                "display_order": 2
+            },
+            "address": {
+                "enabled": true,
+                "properties": {
+                    "city": {
+                        "fields":[{"name":"city","type":"text"}]
+                    }
+                },
+                "display_order": 1
+            }
+        },
+        "display_order":"10"
+    },
+    "beer":{
+        "enabled": true,
+        "properties": {
+            "name": {
+                "fields":[{"name":"name","type":"text"}]
+            }
+        },
+        "display_order":"5"
+    }
+};
+
+var SAMPLE_INDEX_MAPPING = {
+    "types": SAMPLE_TYPE_MAPPING,
+    "default_mapping": {
+        "enabled": true,
+    },
+    "type_field": "_type",
+    "default_type": "_default",
+    "default_analyzer": "standard",
+    "default_datetime_parser": "dateTimeOptional",
+    "default_field": "_all",
+    "byte_array_converter": "json",
+    "analysis": {
+        "analyzers": {},
+        "char_filters": {},
+        "tokenizers": {},
+        "token_filters": {},
+        "token_maps": {
+            "hw": {
+                "type": "custom",
+                "tokens": [
+                    "hello",
+                    "there"
+                ]
+            }
+        }
+    }
+};
+
+angular.module('sampleApp').
+    controller('SampleCtrl', ['$scope', '$http', '$log', '$uibModal',
+    function($scope, $http, $log, $uibModal) {
+        initBleveIndexMappingController(
+            $scope, $http, $log, $uibModal,
+            ['en', 'es', 'keyword', 'standard'],
+            ['julien', 'gregorian', 'yyyymmdd', 'dateTimeOptional'],
+            ['json'],
+            SAMPLE_INDEX_MAPPING);
+    }]);
+
+angular.module('sampleApp').
+    controller('AnalyzerModalCtrl', AnalyzerModalCtrl).
+    controller('CharFilterModalCtrl', CharFilterModalCtrl).
+    controller('TokenizerModalCtrl', TokenizerModalCtrl).
+    controller('TokenFilterModalCtrl', TokenFilterModalCtrl).
+    controller('WordListModalCtrl', WordListModalCtrl);
